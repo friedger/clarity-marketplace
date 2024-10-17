5f0e1b8d7ef1b5999358e09d28c664fc7d9505c1
friedger <mail@friedger.de>
2024-10-17 12:29:41 +0200

    feat: add monster competition

--- contracts/monsters.clar
+++ contracts/monsters.clar
@@ -1,15 +1,13 @@
-(impl-trait 'ST2PABAF9FTAJYNFZH93XENAJ8FVY99RRM4DF2YCW.nft-trait.nft-trait)
-
-(define-map monsters {monster-id: uint}
+(define-map monsters uint
   {name: (string-ascii 20),
   last-meal: uint,
   image: uint,
   date-of-birth: uint}
 )
 
-{name: "text", last-meal: "block-height", image: "number-index", date-of-birth: "block-height"}
+{name: "text", last-meal: "stacks-block-height", image: "number-index", date-of-birth: "stacks-block-height"}
 (define-read-only (get-meta-data (monster-id uint))
-    (map-get? monsters {monster-id: monster-id}))
+    (map-get? monsters monster-id))
 
 
 (define-non-fungible-token nft-monsters uint)
@@ -17,7 +15,7 @@
 (define-constant hunger-tolerance u86400) ;; 1 day in seconds
 
 (define-private (get-time)
-   (unwrap-panic (get-block-info? time (- block-height u1))))
+   (unwrap-panic (get-stacks-block-info? time (- stacks-block-height u1))))
 
 (define-private (is-last-meal-young (last-meal uint))
   (> (to-int last-meal) (to-int (- (get-time) hunger-tolerance))))
@@ -28,7 +26,7 @@
       (if (is-ok (nft-mint? nft-monsters monster-id tx-sender))
         (begin
           (var-set next-id (+ monster-id u1))
-          (map-set monsters {monster-id: monster-id}
+          (map-set monsters monster-id
           {
             name: name,
             last-meal: (get-time),
@@ -40,17 +38,12 @@
 
 {control: "button"}
 (define-public (feed-monster (monster-id uint))
-  (match (map-get? monsters {monster-id: monster-id})
+  (match (map-get? monsters monster-id)
     monster (let ((last-meal (get-time)))
       (if (is-last-meal-young (get last-meal monster))
         (begin
-          (map-set monsters {monster-id: monster-id} {
-            name: (get name monster),
-            last-meal: last-meal,
-            image: (get image monster),
-            date-of-birth: (get date-of-birth monster)
-            })
-          (ok block-height))
+          (map-set monsters monster-id (merge monster {last-meal: last-meal}))
+          (ok stacks-block-height))
         err-monster-died))
     err-monster-unborn))
 
@@ -78,12 +71,69 @@
 )
 
 (define-read-only (is-alive (monster-id uint))
-  (match (map-get? monsters {monster-id: monster-id})
+  (match (map-get? monsters monster-id)
     monster (ok (is-last-meal-young (get last-meal monster)))
    err-monster-unborn
   )
 )
 
+;; competition
+(define-trait tool-trait ((activate () (response bool uint))))
+(define-map winners uint {monster-id: uint, count: uint})
+(define-map second-bests uint {monster-id: uint, count: uint})
+(define-map monster-actions {tenure: uint, monster-id: uint} (list 200 {tool: principal, block: uint}))
+(define-constant no-monster {count: u0, monster-id: u0})
+
+(define-public (use (monster-id uint) (tool (optional <tool-trait>)))
+  (let ((action-key {tenure: tenure-height, monster-id: monster-id})
+        (actions (default-to (list) 
+            (map-get? monster-actions action-key)))
+        (count (+ u1 (len actions)))
+        (current-winner (default-to no-monster (map-get? winners tenure-height))))
+    ;; only owner can use a tool for a monster
+    (asserts! (is-eq (nft-get-owner? nft-monsters monster-id) (some tx-sender)) err-not-owner)
+    ;; register action
+    (map-set monster-actions action-key (unwrap! (as-max-len? (append actions 
+              {tool: (match tool some-tool (contract-of some-tool) (as-contract tx-sender)), 
+                block: stacks-block-height}) 
+              u200) err-too-many-tools))
+    ;; update winner
+    (if (> count (get count current-winner))
+      (let ((new-winner {count: count, monster-id: monster-id}))
+        (print {a: "set-winner", winner: new-winner, tenure: tenure-height})
+        (map-set winners tenure-height new-winner)
+        (print {a: "set-prize", winner: current-winner, tenure: tenure-height})
+        (map-set second-bests tenure-height current-winner))
+      (let ((second-best (default-to no-monster (map-get? second-bests tenure-height))))
+        (if (> count (get count second-best))
+          (let ((new-second-best {count: count, monster-id: monster-id}))
+            (print {a: "set-prize", winner: new-second-best, tenure: tenure-height})
+            (map-set second-bests tenure-height new-second-best))
+          true)))
+    (print {a: "use", tool: tool, count: count, monster-id: monster-id, tenure: tenure-height})
+    ;; activate tool
+    (match tool some-tool (contract-call? some-tool activate) (ok true))
+  )
+)
+
+(define-read-only (get-winner (tenure uint))
+  (map-get? winners tenure))
+
+(define-public (distribute-prize (tenure uint))
+  (let ((second-best (unwrap! (map-get? second-bests tenure) err-no-prize))
+      (prize-id (get monster-id second-best))
+      (winner-owner (unwrap! (nft-get-owner? nft-monsters 
+        (unwrap! (get monster-id (get-winner tenure)) err-monster-unborn)) err-monster-unborn))
+      (prize-owner (unwrap! (nft-get-owner? nft-monsters prize-id) err-monster-unborn)))
+    ;; prize can only be distributed in the next tenure
+    (asserts! (< tenure tenure-height) err-too-early)
+    (if (> prize-id u0)
+      (nft-transfer? nft-monsters prize-id prize-owner winner-owner)
+      (ok true))))
+      
+
+(define-public (distribute-prizes (tenures (list 200 uint)))
+  (ok (map distribute-prize tenures)))
 
 ;; error handling
 (define-constant err-transfer-not-allowed (err u401)) ;; unauthorized
@@ -91,14 +141,10 @@
 (define-constant err-sender-equals-recipient (err u405)) ;; method not allowed
 (define-constant err-monster-exists (err u409)) ;; conflict
 (define-constant err-monster-died (err u501)) ;; internal error
-
-
-(define-map err-strings (response uint uint) (string-ascii 32))
-(map-insert err-strings err-transfer-not-allowed "transfer-not-allowed")
-(map-insert err-strings err-monster-unborn "monster-unborn")
-(map-insert err-strings err-sender-equals-recipient "sender-equals-recipient")
-(map-insert err-strings err-monster-exists "monster-exists")
-(map-insert err-strings err-monster-died "monster-died")
+(define-constant err-too-many-tools (err u502))
+(define-constant err-not-owner (err u503))
+(define-constant err-no-prize (err u504))
+(define-constant err-too-early (err u505))
 
 (define-private (err-nft-transfer (code uint))
   (if (is-eq u1 code)
@@ -113,6 +159,3 @@
   (if (is-eq u1 code)
     err-monster-exists
     (err code)))
-
-(define-read-only (get-errstr (code uint))
-  (unwrap! (map-get? err-strings (err code)) "unknown-error"))
---
+++ tests/clarigen-types.ts
@@ -0,0 +1,361 @@
+
+import type { TypedAbiArg, TypedAbiFunction, TypedAbiMap, TypedAbiVariable, Response } from '@clarigen/core';
+
+export const contracts = {
+  constantTradables: {
+  "functions": {
+    transfer: {"name":"transfer","access":"public","args":[{"name":"tradable-id","type":"uint128"},{"name":"sender","type":"principal"},{"name":"recipient","type":"principal"}],"outputs":{"type":{"response":{"ok":"bool","error":"none"}}}} as TypedAbiFunction<[tradableId: TypedAbiArg<number | bigint, "tradableId">, sender: TypedAbiArg<string, "sender">, recipient: TypedAbiArg<string, "recipient">], Response<boolean, null>>,
+    getOwner: {"name":"get-owner","access":"read_only","args":[{"name":"tradable-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":{"optional":"principal"},"error":"none"}}}} as TypedAbiFunction<[tradableId: TypedAbiArg<number | bigint, "tradableId">], Response<string | null, null>>
+  },
+  "maps": {
+    
+  },
+  "variables": {
+    
+  },
+  constants: {},
+  "non_fungible_tokens": [
+    
+  ],
+  "fungible_tokens":[],"epoch":"Epoch21","clarity_version":"Clarity1",
+  contractName: 'constant-tradables',
+  },
+market: {
+  "functions": {
+    getOwner: {"name":"get-owner","access":"private","args":[{"name":"tradables","type":"trait_reference"},{"name":"tradable-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":{"optional":"principal"},"error":"uint128"}}}} as TypedAbiFunction<[tradables: TypedAbiArg<string, "tradables">, tradableId: TypedAbiArg<number | bigint, "tradableId">], Response<string | null, bigint>>,
+    transferTradableFromEscrow: {"name":"transfer-tradable-from-escrow","access":"private","args":[{"name":"tradables","type":"trait_reference"},{"name":"tradable-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[tradables: TypedAbiArg<string, "tradables">, tradableId: TypedAbiArg<number | bigint, "tradableId">], Response<boolean, bigint>>,
+    transferTradableToEscrow: {"name":"transfer-tradable-to-escrow","access":"private","args":[{"name":"tradables","type":"trait_reference"},{"name":"tradable-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[tradables: TypedAbiArg<string, "tradables">, tradableId: TypedAbiArg<number | bigint, "tradableId">], Response<boolean, bigint>>,
+    accept: {"name":"accept","access":"public","args":[{"name":"tradables","type":"trait_reference"},{"name":"tradable-id","type":"uint128"},{"name":"bid-owner","type":"principal"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[tradables: TypedAbiArg<string, "tradables">, tradableId: TypedAbiArg<number | bigint, "tradableId">, bidOwner: TypedAbiArg<string, "bidOwner">], Response<boolean, bigint>>,
+    bid: {"name":"bid","access":"public","args":[{"name":"tradables","type":"trait_reference"},{"name":"tradable-id","type":"uint128"},{"name":"price","type":"uint128"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[tradables: TypedAbiArg<string, "tradables">, tradableId: TypedAbiArg<number | bigint, "tradableId">, price: TypedAbiArg<number | bigint, "price">], Response<boolean, bigint>>,
+    cancel: {"name":"cancel","access":"public","args":[{"name":"tradables","type":"trait_reference"},{"name":"tradable-id","type":"uint128"},{"name":"bid-owner","type":"principal"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[tradables: TypedAbiArg<string, "tradables">, tradableId: TypedAbiArg<number | bigint, "tradableId">, bidOwner: TypedAbiArg<string, "bidOwner">], Response<boolean, bigint>>,
+    offerTradable: {"name":"offer-tradable","access":"public","args":[{"name":"tradables","type":"trait_reference"},{"name":"tradable-id","type":"uint128"},{"name":"price","type":"uint128"},{"name":"duration","type":"uint128"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[tradables: TypedAbiArg<string, "tradables">, tradableId: TypedAbiArg<number | bigint, "tradableId">, price: TypedAbiArg<number | bigint, "price">, duration: TypedAbiArg<number | bigint, "duration">], Response<boolean, bigint>>,
+    pay: {"name":"pay","access":"public","args":[{"name":"tradables","type":"trait_reference"},{"name":"tradable-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[tradables: TypedAbiArg<string, "tradables">, tradableId: TypedAbiArg<number | bigint, "tradableId">], Response<boolean, bigint>>
+  },
+  "maps": {
+    acceptingOwners: {"name":"accepting-owners","key":{"tuple":[{"name":"tradable-id","type":"uint128"},{"name":"tradables","type":"principal"}]},"value":{"tuple":[{"name":"owner","type":"principal"}]}} as TypedAbiMap<{
+  "tradableId": number | bigint;
+  "tradables": string;
+}, {
+  "owner": string;
+}>,
+    offers: {"name":"offers","key":{"tuple":[{"name":"bid-owner","type":"principal"},{"name":"owner","type":"principal"},{"name":"tradable-id","type":"uint128"},{"name":"tradables","type":"principal"}]},"value":{"tuple":[{"name":"price","type":"uint128"}]}} as TypedAbiMap<{
+  "bidOwner": string;
+  "owner": string;
+  "tradableId": number | bigint;
+  "tradables": string;
+}, {
+  "price": bigint;
+}>,
+    onSale: {"name":"on-sale","key":{"tuple":[{"name":"owner","type":"principal"},{"name":"tradable-id","type":"uint128"},{"name":"tradables","type":"principal"}]},"value":{"tuple":[{"name":"duration","type":"uint128"},{"name":"price","type":"uint128"}]}} as TypedAbiMap<{
+  "owner": string;
+  "tradableId": number | bigint;
+  "tradables": string;
+}, {
+  "duration": bigint;
+  "price": bigint;
+}>
+  },
+  "variables": {
+    errDuplicateEntry: {
+  name: 'err-duplicate-entry',
+  type: 'uint128',
+  access: 'constant'
+} as TypedAbiVariable<bigint>,
+    errInvalidOfferKey: {
+  name: 'err-invalid-offer-key',
+  type: 'uint128',
+  access: 'constant'
+} as TypedAbiVariable<bigint>,
+    errNotAllowed: {
+  name: 'err-not-allowed',
+  type: 'uint128',
+  access: 'constant'
+} as TypedAbiVariable<bigint>,
+    errPaymentFailed: {
+  name: 'err-payment-failed',
+  type: 'uint128',
+  access: 'constant'
+} as TypedAbiVariable<bigint>,
+    errTradableNotFound: {
+  name: 'err-tradable-not-found',
+  type: 'uint128',
+  access: 'constant'
+} as TypedAbiVariable<bigint>,
+    errTransferFailed: {
+  name: 'err-transfer-failed',
+  type: 'uint128',
+  access: 'constant'
+} as TypedAbiVariable<bigint>
+  },
+  constants: {
+  errDuplicateEntry: 5n,
+  errInvalidOfferKey: 1n,
+  errNotAllowed: 4n,
+  errPaymentFailed: 2n,
+  errTradableNotFound: 6n,
+  errTransferFailed: 3n
+},
+  "non_fungible_tokens": [
+    
+  ],
+  "fungible_tokens":[],"epoch":"Epoch21","clarity_version":"Clarity1",
+  contractName: 'market',
+  },
+monsters: {
+  "functions": {
+    errNftMint: {"name":"err-nft-mint","access":"private","args":[{"name":"code","type":"uint128"}],"outputs":{"type":{"response":{"ok":"none","error":"uint128"}}}} as TypedAbiFunction<[code: TypedAbiArg<number | bigint, "code">], Response<null, bigint>>,
+    errNftTransfer: {"name":"err-nft-transfer","access":"private","args":[{"name":"code","type":"uint128"}],"outputs":{"type":{"response":{"ok":"none","error":"uint128"}}}} as TypedAbiFunction<[code: TypedAbiArg<number | bigint, "code">], Response<null, bigint>>,
+    getTime: {"name":"get-time","access":"private","args":[],"outputs":{"type":"uint128"}} as TypedAbiFunction<[], bigint>,
+    isLastMealYoung: {"name":"is-last-meal-young","access":"private","args":[{"name":"last-meal","type":"uint128"}],"outputs":{"type":"bool"}} as TypedAbiFunction<[lastMeal: TypedAbiArg<number | bigint, "lastMeal">], boolean>,
+    createMonster: {"name":"create-monster","access":"public","args":[{"name":"name","type":{"string-ascii":{"length":20}}},{"name":"image","type":"uint128"}],"outputs":{"type":{"response":{"ok":"uint128","error":"uint128"}}}} as TypedAbiFunction<[name: TypedAbiArg<string, "name">, image: TypedAbiArg<number | bigint, "image">], Response<bigint, bigint>>,
+    distributePrize: {"name":"distribute-prize","access":"public","args":[{"name":"tenure","type":"uint128"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[tenure: TypedAbiArg<number | bigint, "tenure">], Response<boolean, bigint>>,
+    distributePrizes: {"name":"distribute-prizes","access":"public","args":[{"name":"tenures","type":{"list":{"type":"uint128","length":200}}}],"outputs":{"type":{"response":{"ok":{"list":{"type":{"response":{"ok":"bool","error":"uint128"}},"length":200}},"error":"none"}}}} as TypedAbiFunction<[tenures: TypedAbiArg<number | bigint[], "tenures">], Response<Response<boolean, bigint>[], null>>,
+    feedMonster: {"name":"feed-monster","access":"public","args":[{"name":"monster-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":"uint128","error":"uint128"}}}} as TypedAbiFunction<[monsterId: TypedAbiArg<number | bigint, "monsterId">], Response<bigint, bigint>>,
+    transfer: {"name":"transfer","access":"public","args":[{"name":"monster-id","type":"uint128"},{"name":"sender","type":"principal"},{"name":"recipient","type":"principal"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[monsterId: TypedAbiArg<number | bigint, "monsterId">, sender: TypedAbiArg<string, "sender">, recipient: TypedAbiArg<string, "recipient">], Response<boolean, bigint>>,
+    use: {"name":"use","access":"public","args":[{"name":"monster-id","type":"uint128"},{"name":"tool","type":{"optional":"trait_reference"}}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[monsterId: TypedAbiArg<number | bigint, "monsterId">, tool: TypedAbiArg<string | null, "tool">], Response<boolean, bigint>>,
+    getLastTokenId: {"name":"get-last-token-id","access":"read_only","args":[],"outputs":{"type":{"response":{"ok":"uint128","error":"none"}}}} as TypedAbiFunction<[], Response<bigint, null>>,
+    getMetaData: {"name":"get-meta-data","access":"read_only","args":[{"name":"monster-id","type":"uint128"}],"outputs":{"type":{"optional":{"tuple":[{"name":"date-of-birth","type":"uint128"},{"name":"image","type":"uint128"},{"name":"last-meal","type":"uint128"},{"name":"name","type":{"string-ascii":{"length":20}}}]}}}} as TypedAbiFunction<[monsterId: TypedAbiArg<number | bigint, "monsterId">], {
+  "dateOfBirth": bigint;
+  "image": bigint;
+  "lastMeal": bigint;
+  "name": string;
+} | null>,
+    getOwner: {"name":"get-owner","access":"read_only","args":[{"name":"monster-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":{"optional":"principal"},"error":"none"}}}} as TypedAbiFunction<[monsterId: TypedAbiArg<number | bigint, "monsterId">], Response<string | null, null>>,
+    getTokenUri: {"name":"get-token-uri","access":"read_only","args":[{"name":"monster-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":{"optional":"none"},"error":"none"}}}} as TypedAbiFunction<[monsterId: TypedAbiArg<number | bigint, "monsterId">], Response<null | null, null>>,
+    getWinner: {"name":"get-winner","access":"read_only","args":[{"name":"tenure","type":"uint128"}],"outputs":{"type":{"optional":{"tuple":[{"name":"count","type":"uint128"},{"name":"monster-id","type":"uint128"}]}}}} as TypedAbiFunction<[tenure: TypedAbiArg<number | bigint, "tenure">], {
+  "count": bigint;
+  "monsterId": bigint;
+} | null>,
+    isAlive: {"name":"is-alive","access":"read_only","args":[{"name":"monster-id","type":"uint128"}],"outputs":{"type":{"response":{"ok":"bool","error":"uint128"}}}} as TypedAbiFunction<[monsterId: TypedAbiArg<number | bigint, "monsterId">], Response<boolean, bigint>>
+  },
+  "maps": {
+    monsterActions: {"name":"monster-actions","key":{"tuple":[{"name":"monster-id","type":"uint128"},{"name":"tenure","type":"uint128"}]},"value":{"list":{"type":{"tuple":[{"name":"block","type":"uint128"},{"name":"tool","type":"principal"}]},"length":200}}} as TypedAbiMap<{
+  "monsterId": number | bigint;
+  "tenure": number | bigint;
+}, {
+  "block": bigint;
+  "tool": string;
+}[]>,
+    monsters: {"name":"monsters","key":"uint128","value":{"tuple":[{"name":"date-of-birth","type":"uint128"},{"name":"image","type":"uint128"},{"name":"last-meal","type":"uint128"},{"name":"name","type":{"string-ascii":{"length":20}}}]}} as TypedAbiMap<number | bigint, {
+  "dateOfBirth": bigint;
+  "image": bigint;
+  "lastMeal": bigint;
+  "name": string;
+}>,
+    secondBests: {"name":"second-bests","key":"uint128","value":{"tuple":[{"name":"count","type":"uint128"},{"name":"monster-id","type":"uint128"}]}} as TypedAbiMap<number | bigint, {
+  "count": bigint;
+  "monsterId": bigint;
+}>,
+    winners: {"name":"winners","key":"uint128","value":{"tuple":[{"name":"count","type":"uint128"},{"name":"monster-id","type":"uint128"}]}} as TypedAbiMap<number | bigint, {
+  "count": bigint;
+  "monsterId": bigint;
+}>
+  },
+  "variables": {
+    errMonsterDied: {
+  name: 'err-monster-died',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    errMonsterExists: {
+  name: 'err-monster-exists',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    errMonsterUnborn: {
+  name: 'err-monster-unborn',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    errNoPrize: {
+  name: 'err-no-prize',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    errNotOwner: {
+  name: 'err-not-owner',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    errSenderEqualsRecipient: {
+  name: 'err-sender-equals-recipient',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    errTooEarly: {
+  name: 'err-too-early',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    errTooManyTools: {
+  name: 'err-too-many-tools',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    errTransferNotAllowed: {
+  name: 'err-transfer-not-allowed',
+  type: {
+    response: {
+      ok: 'none',
+      error: 'uint128'
+    }
+  },
+  access: 'constant'
+} as TypedAbiVariable<Response<null, bigint>>,
+    hungerTolerance: {
+  name: 'hunger-tolerance',
+  type: 'uint128',
+  access: 'constant'
+} as TypedAbiVariable<bigint>,
+    noMonster: {
+  name: 'no-monster',
+  type: {
+    tuple: [
+      {
+        name: 'count',
+        type: 'uint128'
+      },
+      {
+        name: 'monster-id',
+        type: 'uint128'
+      }
+    ]
+  },
+  access: 'constant'
+} as TypedAbiVariable<{
+  "count": bigint;
+  "monsterId": bigint;
+}>,
+    nextId: {
+  name: 'next-id',
+  type: 'uint128',
+  access: 'variable'
+} as TypedAbiVariable<bigint>
+  },
+  constants: {
+  errMonsterDied: {
+    isOk: false,
+    value: 501n
+  },
+  errMonsterExists: {
+    isOk: false,
+    value: 409n
+  },
+  errMonsterUnborn: {
+    isOk: false,
+    value: 404n
+  },
+  errNoPrize: {
+    isOk: false,
+    value: 504n
+  },
+  errNotOwner: {
+    isOk: false,
+    value: 503n
+  },
+  errSenderEqualsRecipient: {
+    isOk: false,
+    value: 405n
+  },
+  errTooEarly: {
+    isOk: false,
+    value: 505n
+  },
+  errTooManyTools: {
+    isOk: false,
+    value: 502n
+  },
+  errTransferNotAllowed: {
+    isOk: false,
+    value: 401n
+  },
+  hungerTolerance: 86_400n,
+  nextId: 1n,
+  noMonster: {
+    count: 0n,
+    monsterId: 0n
+  }
+},
+  "non_fungible_tokens": [
+    {"name":"nft-monsters","type":"uint128"}
+  ],
+  "fungible_tokens":[],"epoch":"Epoch30","clarity_version":"Clarity3",
+  contractName: 'monsters',
+  },
+tradablesTrait: {
+  "functions": {
+    
+  },
+  "maps": {
+    
+  },
+  "variables": {
+    
+  },
+  constants: {},
+  "non_fungible_tokens": [
+    
+  ],
+  "fungible_tokens":[],"epoch":"Epoch21","clarity_version":"Clarity1",
+  contractName: 'tradables-trait',
+  }
+} as const;
+
+export const accounts = {"deployer":{"address":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM","balance":"100000000000000"},"faucet":{"address":"STNHKEPYEPJ8ET55ZZ0M5A34J0R3N5FM2CMMMAZ6","balance":"100000000000000"},"wallet_1":{"address":"ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5","balance":"100000000000000"},"wallet_2":{"address":"ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG","balance":"100000000000000"},"wallet_3":{"address":"ST2JHG361ZXG51QTKY2NQCVBPPRRE2KZB1HR05NNC","balance":"100000000000000"},"wallet_4":{"address":"ST2NEB84ASENDXKYGJPQW86YXQCEFEX2ZQPG87ND","balance":"100000000000000"},"wallet_5":{"address":"ST2REHHS5J3CERCRBEPMGH7921Q6PYKAADT7JP2VB","balance":"100000000000000"},"wallet_6":{"address":"ST3AM1A56AK2C1XAFJ4115ZSV26EB49BVQ10MGCS0","balance":"100000000000000"},"wallet_7":{"address":"ST3PF13W7Z0RRM42A8VZRVFQ75SV1K26RXEP8YGKJ","balance":"100000000000000"},"wallet_8":{"address":"ST3NBRSFKX28FQ2ZJ1MAKX58HKHSDGNV5N7R21XCP","balance":"100000000000000"}} as const;
+
+export const identifiers = {"constantTradables":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.constant-tradables","market":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.market","monsters":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.monsters","tradablesTrait":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.tradables-trait"} as const
+
+export const simnet = {
+  accounts,
+  contracts,
+  identifiers,
+} as const;
+
+
+export const deployments = {"constantTradables":{"devnet":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.constant-tradables","simnet":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.constant-tradables","testnet":null,"mainnet":null},"market":{"devnet":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.market","simnet":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.market","testnet":null,"mainnet":null},"monsters":{"devnet":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.monsters","simnet":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.monsters","testnet":null,"mainnet":null},"tradablesTrait":{"devnet":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.tradables-trait","simnet":"ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.tradables-trait","testnet":null,"mainnet":null}} as const;
+
+export const project = {
+  contracts,
+  deployments,
+} as const;
+  
